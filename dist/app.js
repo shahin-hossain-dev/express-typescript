df2af3fe26dc29a637549d87dbe84809e914033d
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2025-04-23 01:33:05 +0600

    parsers, request and response object

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+//parser middleware
+app.use(express_1.default.json()); //json file parse করার জন্য ব্যবহার করা হয়।
+app.use(express_1.default.text()); //plain text parse করার জন্য ব্যবহার করা হয়।
 app.get("/", (req, res) => {
     res.send("Express Server is Running with nodemon ");
+});
+app.post("/another", (req, res) => {
+    console.log(req.body);
+    //   res.send("Successful hit");
+    res.json({
+        message: "Successfully received data",
+    });
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+//parser middleware
+app.use(express.json()); //json file parse করার জন্য ব্যবহার করা হয়।
+app.use(express.text()); //plain text parse করার জন্য ব্যবহার করা হয়।
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Express Server is Running with nodemon ");
+});
+
+app.post("/another", (req: Request, res: Response) => {
+  console.log(req.body);
+
+  //   res.send("Successful hit");
+  res.json({
+    message: "Successfully received data",
+  });
 });
 
 export default app;
