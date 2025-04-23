b87ed0c23723832c144246db53f67e7e62aa1345
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2025-04-23 22:55:39 +0600

    middleware in express.js

--- dist/app.js
+++ dist/app.js
@@ -8,14 +8,36 @@ const app = (0, express_1.default)();
 //parser middleware
 app.use(express_1.default.json()); //json file parse করার জন্য ব্যবহার করা হয়।
 app.use(express_1.default.text()); //plain text parse করার জন্য ব্যবহার করা হয়।
-app.get("/", (req, res) => {
-    res.send("Express Server is Running with nodemon ");
+//CUSTOM MIDDLEWARE
+const logger = (req, res, next) => {
+    req.body = "Shahin";
+    next();
+};
+//get params value
+app.get("/users/:userId/:storeId", (req, res) => {
+    const { userId, storeId } = req === null || req === void 0 ? void 0 : req.params; //get params
+    res.json({ message: `user id = ${userId}, store id = ${storeId}` });
 });
-app.post("/another", (req, res) => {
+//get query values
+app.get("/users", (req, res) => {
+    const { email } = req.query;
+    console.log(email);
+    res.json({ email });
+});
+//use middleware
+app.post("/another", logger, (req, res) => {
     console.log(req.body);
+    console.log(req.params);
     //   res.send("Successful hit");
     res.json({
         message: "Successfully received data",
     });
 });
 exports.default = app;
+/**
+ * middleware
+ * middleware হলো একটা function যা কোনো request এবং response এর মাঝামাঝি বসে কিছু অপারেশন করে
+ * next() function এর মাধম্যে কল করে, কল করলে সে request function এর মধ্যে ঢুকবে।
+ * যদি middleware function condition না মিলে তখন middleware থেকে error response করে।
+ * request route এর মধ্যে ঢুকবে না। এটাই হলো middleware.
+ */
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
@@ -6,13 +6,31 @@ const app = express();
 app.use(express.json()); //json file parse করার জন্য ব্যবহার করা হয়।
 app.use(express.text()); //plain text parse করার জন্য ব্যবহার করা হয়।
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express Server is Running with nodemon ");
+//CUSTOM MIDDLEWARE
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  req.body = "Shahin";
+  next();
+};
+
+//get params value
+app.get("/users/:userId/:storeId", (req: Request, res: Response) => {
+  const { userId, storeId } = req?.params; //get params
+  res.json({ message: `user id = ${userId}, store id = ${storeId}` });
 });
 
-app.post("/another", (req: Request, res: Response) => {
-  console.log(req.body);
+//get query values
+app.get("/users", (req: Request, res: Response) => {
+  const { email } = req.query;
+  console.log(email);
+  res.json({ email });
+});
 
+//use middleware
+
+app.post("/another", logger, (req: Request, res: Response) => {
+  console.log(req.body);
+  console.log(req.params);
   //   res.send("Successful hit");
   res.json({
     message: "Successfully received data",
@@ -20,3 +38,11 @@ app.post("/another", (req: Request, res: Response) => {
 });
 
 export default app;
+
+/**
+ * middleware
+ * middleware হলো একটা function যা কোনো request এবং response এর মাঝামাঝি বসে কিছু অপারেশন করে
+ * next() function এর মাধম্যে কল করে, কল করলে সে request function এর মধ্যে ঢুকবে।
+ * যদি middleware function condition না মিলে তখন middleware থেকে error response করে।
+ * request route এর মধ্যে ঢুকবে না। এটাই হলো middleware.
+ */
